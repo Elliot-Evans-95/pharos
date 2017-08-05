1f41b0cf7f0167074b6149a0f7b7b6854c02b32c
Elliot Evans <ell15evans.nuls@googlemail.com>
2017-08-05 22:56:49 +0100

    added json ouput for the project and added site / output flags

--- list.js
+++ list.js
@@ -7,6 +7,8 @@ const projectVersion = require('./package.json').version;
 
 program
     .version(projectVersion)
+    .option('-s, --sites <sites>', 'What url(s) should lighthouse run')
+    .option('-out, --output <output>', 'To determine what type of output you would like')
     .parse(process.argv);
 
 runLighthouse(program);
--- run.js
+++ run.js
@@ -5,33 +5,39 @@ const async = require('async');
 
 module.exports = runLighthouse;
 
+function createSitesArray(sites) {
+    return sites.split(',');
+}
+
 function runLighthouse(userOptions) {
 
-    if (userOptions.args.length === 0 || userOptions.args.length === null || userOptions.args.length === undefined) {
+    if (userOptions.sites === null || userOptions.sites === undefined) {
         throw "You must pass in a Site URL";
     }
 
-    anotherOne(userOptions);
+    let siteArray = createSitesArray(userOptions.sites);
+    anotherOne(userOptions.output, siteArray);
 }
 
 function splitArray (site) {
+    console.log('Sites:', site);
     return site.replace(",", "");
 }
 
 function RunLighthouseException(error) {
-    this.toString = function() {
-        return "Something went wrong when running Lighthouse:" + error;
-    };
+    throw `Something went wrong when running Lighthouse: ${error}`;
 }
 
-function anotherOne(options) {
-    for(let i = 0; i < options.args.length; i++) {
-        let formattedSites = splitArray(options.args[i]);
+function anotherOne(output, sites) {
+    for(let i = 0; i < sites.length; i++) {
+        let formattedSites = sites[i];
+        let outputName = formattedSites.replace(/\//g, '');
+        console.log('outputName', outputName);
 
         async.parallel([
             function () {
                 cmd.get(
-                    `lighthouse ${formattedSites} --quiet`,
+                    `lighthouse ${formattedSites} --quiet --output=${output} --output-path=${outputName}.${output}`,
                     function(error){
                         if (error) {
                             throw new RunLighthouseException(error);
