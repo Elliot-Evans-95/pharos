d91ec91a9da8140f6a554a2b9bcdfe12be34d859
Elliot Evans <ell15evans.nuls@googlemail.com>
2017-09-03 12:57:36 +0100

    added taskr as a build step

---
+++ src/run.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const cmd = require('node-cmd');
+const async = require('async');
+
+module.exports = executeLighthouse;
+
+function createSitesArray(sites) {
+    return sites.split(',');
+}
+
+function executeLighthouse(userOptions) {
+
+    if (userOptions.sites === null || userOptions.sites === undefined) {
+        console.error("You must pass in a Site URL");
+        process.exit(1);
+    }
+
+    let siteArray = createSitesArray(userOptions.sites);
+    runCustomLighthouse(userOptions.output, siteArray);
+}
+
+function RunLighthouseException(error) {
+    console.error(`Something went wrong when running Lighthouse: ${error}`);
+    process.exit(1);
+}
+
+function runCustomLighthouse(output, sites) {
+    for(let i = 0; i < sites.length; i++) {
+        let _formattedSites = sites[i];
+        let _outputName = _formattedSites.replace(/\//g, '');
+        let _outputFile = `${_outputName}.${output}`;
+
+        async.parallel([
+            function () {
+                cmd.get(
+                    `lighthouse ${_formattedSites} --quiet --output=${output} --output-path=${_outputFile} --chrome-flags="--headless"`,
+                    function(error){
+                        if (error) {
+                            throw new RunLighthouseException(error);
+                        }
+                        require('lighthouse-quaggy')(_formattedSites, _outputFile);
+                    }
+                )
+            }
+        ], function (error) {
+            throw new RunLighthouseException(error);
+        });
+    }
+}
---
+++ taskfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+    *clear(task) {
+        yield task.clear(['dist', 'build']);
+    },
+    *minify(task) {
+        yield task.source(['src/list.js', 'src/run.js'])
+            .babel({ presets:['es2015'] })
+            .uglify({
+                compress: {
+                    drop_console: true,
+                    join_vars: true
+                }
+            })
+            .concat({ output:'all.js', map:true })
+            .target('dist')
+    },
+    *build(task) {
+        yield task.parallel(['clear', 'minify',]);
+    }
+};
